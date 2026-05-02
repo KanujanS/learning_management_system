facd5041c0bc5f003e479fcb6fa5bad5338a918f
KanujanS <kanujansk@gmail.com>
2026-05-02 23:45:18 +0530

    better auth, prisma initialized

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,36 @@
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
+import Logo1 from "@/public/logo1.png";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
-      <Link href="/" className={buttonVariants({ variant: 'outline', className: "absolute top-4 left-4"})}>
+      <Link
+        href="/"
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute top-4 left-4",
+        })}
+      >
         <ArrowLeft className="size-4" />
         Back
       </Link>
-      <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <Image src={Logo1} alt="Logo" width={40} height={40} />
+          <span className="text-lg font-bold">EduFlow</span>
+        </Link>
+        {children}
+        <div className="text-balance text-center text-xs text-muted-foreground">
+            By clicking continue, you agree to our <span className="hover:text-primary hover:underline cursor-pointer">Terms of service</span> {""} and <span className="hover:text-primary hover:underline cursor-pointer">Privacy policy</span>.
+        </div>
+      </div>
     </div>
   );
 }
