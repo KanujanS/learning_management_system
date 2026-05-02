637cc32440e85ad86f7b8a1cd577bb5c7f6abb0b
KanujanS <kanujansk@gmail.com>
2026-05-02 13:07:25 +0530

    Enhance login page layout with additional input fields and styling

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,19 +6,31 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function LoginPage() {
   return (
     <div>
       <Card>
         <CardHeader>
-          <CardTitle>Welcome back!</CardTitle>
+          <CardTitle className="text-lg font-bold">Welcome back!</CardTitle>
           <CardDescription>
             Login with your GitHub Email Account
           </CardDescription>
         </CardHeader>
-        <CardContent>
+        <CardContent className="flex flex-col gap-4">
           <Button className="w-full" variant={"outline"}>Sign In With GitHub</Button>
+          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            <span className="relative z-10 bg-card px-2 text-muted-foreground">Or continue with</span>
+          </div>
+          <div className="grid gap-3">
+            <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" placeholder="m@example.com"/>
+            </div>
+            <Button>Continue With Email</Button>
+          </div>
         </CardContent>
       </Card>
     </div>
